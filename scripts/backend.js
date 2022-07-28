ba3be0f148607a41a676d264a890e7817b8c97fc
lembn <61047330+lembn@users.noreply.github.com>
2022-07-28 19:39:50 +0100

    backend.js v1

--- scripts/backend.js
+++ scripts/backend.js
@@ -1,29 +1,25 @@
 require("dotenv").config();
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
+const winston = require("winston");
 
-const dockerrc = {
-  linux: `docker run --rm --name ${process.env.MOONBEAM_CONTAINER_NAME} --network host purestake/moonbeam:v${process.env.MOONBEAM_VERSION} --dev`,
-  darwin: `docker run --rm --name ${process.env.MOONBEAM_CONTAINER_NAME} -p 9944:9944 -p 9933:9933 purestake/moonbeam:v${process.env.MOONBEAM_VERSION} --dev --ws-external --rpc-external`,
-  win32: `docker run --rm --name ${process.env.MOONBEAM_CONTAINER_NAME} -p 9944:9944 -p 9933:9933 purestake/moonbeam:v${process.env.MOONBEAM_VERSION} --dev --ws-external --rpc-external`,
-};
+// TODO: use winston logging
+// TODO: suppress ouput of exec when node is closed
 
-// TODO: make command param accept single and multiple values, so we don't keep having to use for...of
-async function execute(command, log = true) {
-  if (log) console.log("-- " + command);
+async function execute(command, print = true) {
+  if (print) console.log("-- " + arguments);
   const { stdout, stderr } = await exec(command);
   if (stderr) throw stderr;
-  if (log) console.log(stdout);
   return stdout;
 }
 
-function exitHandler(options, exitCode) {
-  if (options.cleanup) console.log("clean");
-  if (exitCode || exitCode === 0) console.log(exitCode);
-  if (options.exit) process.exit();
-}
-
 async function main() {
+  const dockerrc = {
+    linux: `docker run --rm --name ${process.env.MOONBEAM_CONTAINER_NAME} --network host purestake/moonbeam:v${process.env.MOONBEAM_VERSION} --dev`,
+    darwin: `docker run --rm --name ${process.env.MOONBEAM_CONTAINER_NAME} -p 9944:9944 -p 9933:9933 purestake/moonbeam:v${process.env.MOONBEAM_VERSION} --dev --ws-external --rpc-external`,
+    win32: `docker run --rm --name ${process.env.MOONBEAM_CONTAINER_NAME} -p 9944:9944 -p 9933:9933 purestake/moonbeam:v${process.env.MOONBEAM_VERSION} --dev --ws-external --rpc-external`,
+  };
+
   if (!(process.platform in dockerrc)) {
     console.error("ERROR: Unsupported OS");
     return;
@@ -31,25 +27,38 @@ async function main() {
 
   console.log("Deploying local Moonbeam node in development node...");
   console.log("[[LAUNCHING]]");
-  const commands = [
-    `docker pull purestake/moonbeam:v${process.env.MOONBEAM_VERSION}`,
-    dockerrc[process.platform],
-  ];
-
-  for (const command of commands) await execute(command); // code gets stuck here because the `docker run` command doesn't return
-
-  const containerId = await execute(
-    `docker ps -aqf "name=^${process.env.MOONBEAM_CONTAINER_NAME}$"`
+  await execute(
+    `docker pull purestake/moonbeam:v${process.env.MOONBEAM_VERSION}`
   );
+  execute(dockerrc[process.platform]);
+
+  console.log("Detecting container...");
+  const containerID = await new Promise(async (resolve, reject) => {
+    async function getCID(timeWaited = 0) {
+      if (timeWaited > 20000) reject("Took too long to get container ID");
+
+      const cid = await execute(
+        `docker ps -aqf "name=^${process.env.MOONBEAM_CONTAINER_NAME}$"`,
+        false
+      );
+      if (cid) resolve(cid.trim());
+      else setTimeout(getCID, 100, timeWaited + 100);
+    }
+
+    await getCID();
+  });
 
   console.log(
-    `Local Moonbeam development mode running in contianer ${process.env.MOONBEAM_CONTAINER_NAME}-${containerId}`
+    `[[ACTIVE]] - Local development node running in [${process.env.MOONBEAM_CONTAINER_NAME}-${containerID}]`
   );
-
-  process.on("exit", () => {
-    const commands = [`docker stop ${containerId}`, `docker rm ${containerId}`]
-    for (const command of commands)
-      await execute(process.platform == "linux" ? "sudo " : "" + command)
+  process.on("SIGINT", async () => {
+    console.log("Closing local development node...");
+    const commands = [
+      `docker stop ${containerID}`,
+      `docker rm ${containerID}`,
+    ].map((cmd) => (process.platform == "linux" ? "sudo " : "" + cmd));
+    for (const command of commands) await execute(command, false);
+    process.exit(0);
   });
 }
 
