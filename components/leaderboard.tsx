be62e861396621a30108c7e500bc278c375f5474
Narges <nrahm77@gmail.com>
2022-07-30 21:11:59 +0100

    sorted list

---
+++ components/leaderboard.tsx
@@ -0,0 +1,12 @@
+import Leaderboard_list from './Leaderboard_list';
+
+function Leaderboard() {
+  return (
+    <div className="container">
+      <h1>Leaderboard</h1>
+      <Leaderboard_list />
+    </div>
+  );
+}
+
+export default Leaderboard;
