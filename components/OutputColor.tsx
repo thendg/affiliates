4351f681b6b111cf0bcb2908f890f13ecfbdc78a
Nathanael Abebe Assefa <nathanaelsergut@yahoo.co.uk>
2022-07-31 15:10:28 +0100

    natty

---
+++ components/OutputColor.tsx
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import Background from './Background';
+
+export default function OutputColour({
+  children,
+  address,
+}: {
+  children: JSX.Element,
+  address: string,
+}) {
+  let randomColor = () => {
+    let hexCode = '#';
+    for (let i = 0; i < 6; i++) {
+      hexCode += hexString[Math.floor(Math.random() * hexString.length)];
+    }
+    return hexCode;
+  };
+  {
+    /*
+  function colourOnAddress(address: string) {
+    let cleanFullAddress = address.slice(2);
+    let address1 = cleanFullAddress.substring(0, 20);
+    let address2 = cleanFullAddress.substring(0, 40);
+    let hexchars = [
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+    ];
+    let result = '#';
+    let result2 = '#';
+    for (let i = 0; i < address1.length && result.length < 8; i++) {
+      if (hexchars.includes(address1[i])) {
+        result += address1[i];
+      }
+    }
+    for (let i = 0; i < address2.length && result.length < 8; i++) {
+      if (hexchars.includes(address2[i])) {
+        result2 += address2[i];
+      }
+    }
+    return [result, result2];
+  }
+*/
+  }
+  let hexString = '0123456789abcdef';
+  let colorOne = randomColor();
+  let colorTwo = randomColor();
+  let angle = Math.floor(Math.random() * 360);
+
+  //   const Container = styled.div`
+  //     background-image: linear-gradient(${angle}deg, ${colorOne}, ${colorTwo});
+  //   `;
+
+  return (
+    <Background angle={angle} colorOne={colorOne} colorTwo={colorTwo}>
+      {children}
+    </Background>
+  );
+}
--- pages/members.tsx
+++ pages/members.tsx
@@ -1,5 +1,61 @@
+import { callbackify } from 'util';
+import OutputColour from '../components/OutputColor';
+import styled from 'styled-components';
+
+const Hover = styled.h1`
+  p {
+    padding: 2rem;
+    text-align: center;
+    width: 100%;
+  }
+  .w_address {
+    text-align: center;
+    font-family: 'Montserrat';
+  }
+  .change:hover p.w_address b {
+    display: none;
+  }
+  .change:hover p.w_address:after {
+    content: '944E...F353';
+    font-family: 'Montserrat';
+    font-style: italic;
+    padding: 2rem;
+  }
+`;
+
+const members: JSX.Element[] = [];
+for (let i = 0; i < 20; i++) {
+  members.push(
+    <div
+      key={'membercomp' + i}
+      className="m-8 border-1 border-black duration-700 w-56 shadow-md hover:shadow-xl"
+    >
+      <OutputColour
+        key={'oc' + i}
+        address={'0x76a8Ad01192213C8faE291E5DaaE2fBC8ce74FEb'}
+      >
+        <p className="font- montserrat text-white text-right text-xl p-2">
+          <b>900</b>
+        </p>
+      </OutputColour>
+      <Hover key={'h' + i}>
+        <div className="change">
+          <p className="w_address">
+            <h4>lembn</h4>
+            <b>President</b>
+          </p>
+        </div>
+      </Hover>
+    </div>
+  );
+}
+
 function Members() {
-  return <h1>hello</h1>;
+  return (
+    <div>
+      <div className="grid grid-cols-5 gap-4">{members}</div>
+    </div>
+  );
 }
 
 export default Members;
