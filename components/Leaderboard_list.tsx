665bcc82dd7d69ae3fcad07331372ab6ce3394cd
Narges <nrahm77@gmail.com>
2022-07-31 02:34:23 +0100

    dummy data and merge with Miron's homepage

--- components/Leaderboard_list.tsx
+++ components/Leaderboard_list.tsx
@@ -1,31 +1,39 @@
 import { useState } from 'react';
 
+//dummy list
 const Leaderboard_list = () => {
   const [leaderboard_list, setLeaderboard] = useState([
     {
       id: 1,
       username: 'Narges',
       walletAddress: 1,
-      memberships: [],
+      points: 10,
     },
     {
       id: 2,
       username: 'John',
       walletAddress: 2,
-      memberships: [],
+      points: 20,
     },
     {
       id: 3,
       username: 'Jane',
       walletAddress: 3,
-      memberships: [],
+      points: 5,
     },
   ]);
 
+  //sort list based on points.
+  const sorted = leaderboard_list.sort((a, b) => b.points - a.points);
+  let count: number = 1;
+
+  //format and return text.
   return (
     <>
       {leaderboard_list.map(user => (
-        <h3 key={user.id}>{user.username}</h3>
+        <h3 key={user.id}>
+          {count++} {user.username} {user.points}
+        </h3>
       ))}
     </>
   );
